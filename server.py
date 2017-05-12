c5074d1448440f9a45fd7a6da41a7d73a2d17841
Thomas Gonzalez <gonzalez.thomas@icloud.com>
2017-05-12 14:51:08 +0200

    Requete post lié à la fonction de retour sparql

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-from services import request_sparql
+from services import Services
 
 app = Flask(__name__)
 
@@ -16,9 +16,10 @@ def form():
 # Result List
 @app.route('/result', methods=['POST'])
 def result():
-    author=request.form['author']
-    genre=request.form['genre']
-    results = request_sparql(author, genre)
+    author = request.form['author']
+    genre = request.form['genre']
+    service = Services()
+    results = service.request_sparql(author, genre)
     return render_template('form_action.html', author=author, genre=genre, results=results)
 
 
--- services.py
+++ services.py
@@ -5,49 +5,52 @@ import rdflib
 from rdflib.plugins.sparql import prepareQuery
 from rdflib import Namespace
 
-PATH = "../FichiersMusicaux/"
-EXTENSION = ".wav"
-g = rdflib.Graph()
-name_space_artist = Namespace("http://ns.adobe.com/xmp/1.0/DynamicMedia/")
 
+class Services:
+    def __init__(self):
+        self.PATH = "/Users/thomas/Documents/Master/Digital Content Broadcasting/untitled/FichiersMusicaux/"
+        self.EXTENSION = ".wav"
+        self.name_space_artist = Namespace("http://ns.adobe.com/xmp/1.0/DynamicMedia/")
+        self.g = rdflib.Graph()
+        self.list_all_files()
 
-def read_xmp_metadata(file):
-    # Read file
-    xmpfile = libxmp.XMPFiles(file_path=file, open_forupdate=False)
-    # Get XMP from file
-    xmp = xmpfile.get_xmp()
-    xmp = str(xmp)
-    xmp = "<rdf:RDF" + xmp.split("<rdf:RDF",1)[1]
-    xmp = xmp.split("</rdf:RDF>", 1)[0] + "</rdf:RDF>"
-    print xmp
-    xmpfile.close_file()
-    return xmp
+    def read_xmp_metadata(self, file):
+        # Read file
+        xmpfile = libxmp.XMPFiles(file_path=file, open_forupdate=False)
+        # Get XMP from file
+        xmp = xmpfile.get_xmp()
+        xmp = str(xmp)
+        xmp = "<rdf:RDF" + xmp.split("<rdf:RDF", 1)[1]
+        xmp = xmp.split("</rdf:RDF>", 1)[0] + "</rdf:RDF>"
+        print xmp
+        xmpfile.close_file()
+        return xmp
 
+    def import_graph(self, rdf):
+        temp = tempfile.NamedTemporaryFile()
+        temp.write(rdf)
+        temp.seek(0)
+        self.g.load(temp.name)
+        for s, p, o in self.g:
+            print s, p, o
+        temp.close()
 
-def list_all_files():
-    print("--- start list_all_files() ---")
-    for root, dirs, files in os.walk(PATH):
-        for name in files:
-            if name.endswith(EXTENSION):
-                print("--- read_xmp_metadata : " + name + " ---")
-                import_graph(read_xmp_metadata(root + "/" + name))
-    print("--- end list_all_files() ---")
+    def list_all_files(self):
+        print("--- start list_all_files() ---")
+        for root, dirs, files in os.walk(self.PATH):
+            for name in files:
+                if name.endswith(self.EXTENSION):
+                    print("--- read_xmp_metadata : " + name + " ---")
+                    self.import_graph(self.read_xmp_metadata(root + "/" + name))
+        print("--- end list_all_files() ---")
 
-
-def import_graph(rdf):
-    temp = tempfile.NamedTemporaryFile()
-    temp.write(rdf)
-    temp.seek(0)
-    g.load(temp.name)
-    for s, p, o in g:
-        print s, p, o
-    temp.close()
-
-
-def request_sparql(auteur, genre):
-    list_all_files()
-    q = prepareQuery('SELECT * WHERE { ?s xmpDM:artist "' + auteur + '" . ?s xmpDM:genre "' + genre + '". ?s ?predicate ?object . }', initNs={"xmpDM": name_space_artist})
-    #Show result
-    #for row in g.query(q):
-    #    print row
-    return g.query(q)
+    def request_sparql(self, auteur, genre):
+        q = prepareQuery(
+            'SELECT * WHERE { ?s xmpDM:artist "' + auteur + '" . ?s xmpDM:genre "' + genre + '". ?s ?predicate ?object . }',
+            initNs={"xmpDM": self.name_space_artist})
+        # Show result
+        a = []
+        for b, c, d in self.g.query(q):
+            print b, c, d
+            a.append([b, c, d])
+        return a
